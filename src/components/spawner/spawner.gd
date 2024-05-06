a29c18d020ee0e05fec33a77104cbcdd6e3bf8b9
SytonK <122539345+SytonK@users.noreply.github.com>
2024-05-06 18:52:51 +0300

    spawner/allow to spawn new scenes

--- src/components/spawner/spawner.gd
+++ src/components/spawner/spawner.gd
@@ -1,8 +1,10 @@
 class_name Spawner
 extends Node2D
 
-@export var frequency: float = 1
+@export var frequency: float = 1 : set = _set_frequency
 var frequency_timer: Timer
+
+@export var scene_to_spawn: PackedScene
 
 func _ready():
 	_init_frequency_timer()
@@ -17,4 +19,11 @@ func _init_frequency_timer() -> void:
 
 
 func _spawn() -> void:
-	print('test')
+	var new_scene: = scene_to_spawn.instantiate()
+	add_child(new_scene)
+
+
+func _set_frequency(new_frequency: float) -> void:
+	if frequency_timer && new_frequency > 0:
+		frequency = new_frequency
+		frequency_timer.wait_time = frequency
