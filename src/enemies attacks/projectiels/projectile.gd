3fedab6893c3f329eda3e242cd4f04763dcd93a8
SytonK <122539345+SytonK@users.noreply.github.com>
2024-05-07 08:54:43 +0300

    Projectile/movement

---
+++ src/enemies attacks/projectiels/projectile.gd
@@ -0,0 +1,17 @@
+class_name Projectile
+extends Hitbox
+
+
+@export var speed: float = 300
+@export var direction: Vector2 : set = _set_direction
+
+
+func _process(delta: float) -> void:
+	position += direction * speed * delta
+
+
+func _set_direction(new_direction: Vector2) -> void:
+	if new_direction == Vector2.ZERO:
+		queue_free()
+	else:
+		direction = new_direction.normalized()
