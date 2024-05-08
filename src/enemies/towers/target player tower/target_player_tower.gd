0305ecc4a7f5c516f39f324fc055b764009bb26a
SytonK <122539345+SytonK@users.noreply.github.com>
2024-05-08 18:35:53 +0300

    TargetPlayerTower/set color

--- src/enemies/towers/target player tower/target_player_tower.gd
+++ src/enemies/towers/target player tower/target_player_tower.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 
+const COLOR_MODDLATE_FACTOR: float = 0.001
+
+
 @export var projectile_speed: int = 300
 
 
@@ -8,3 +11,6 @@ func _on_spawner_scene_spawned(projectile: Projectile) -> void:
 	projectile.direction = Globals.player_ref.position - projectile.position
 	projectile.rotation = projectile.direction.angle()
 	projectile.speed = projectile_speed
+	projectile.modulate = Color(0.8, 
+		0.8 - projectile.speed * COLOR_MODDLATE_FACTOR, 
+		0.8 - projectile.speed * COLOR_MODDLATE_FACTOR)
