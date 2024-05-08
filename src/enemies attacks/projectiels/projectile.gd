0a695ac05da9346a23dbeb02cc5e670ac6018fee
SytonK <122539345+SytonK@users.noreply.github.com>
2024-05-08 20:13:34 +0300

    Projectile/free on max range

--- src/enemies attacks/projectiels/projectile.gd
+++ src/enemies attacks/projectiels/projectile.gd
@@ -5,9 +5,17 @@ extends Hitbox
 @export var speed: float = 300
 @export var direction: Vector2 : set = _set_direction
 
+@export var max_distance_from_axis: int = 5000
+
 
 func _process(delta: float) -> void:
 	position += direction * speed * delta
+	_queue_free_over_max_distance()
+
+
+func _queue_free_over_max_distance() -> void:
+	if abs(global_position.x) > max_distance_from_axis or abs(global_position.y) > max_distance_from_axis:
+		queue_free()
 
 
 func _set_direction(new_direction: Vector2) -> void:
