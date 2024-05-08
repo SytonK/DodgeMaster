008160910c658ab07ca1e3ea48c0fc97acb4fd03
SytonK <122539345+SytonK@users.noreply.github.com>
2024-05-08 23:19:29 +0300

    Sandbox/max distance from axis

--- src/enemies/towers/target player tower/target_player_tower.gd
+++ src/enemies/towers/target player tower/target_player_tower.gd
@@ -5,6 +5,7 @@ const COLOR_MODDLATE_FACTOR: float = 0.001
 
 
 @export var projectile_speed: int = 300
+@export var max_distance_from_axis: int
 
 
 func _on_spawner_scene_spawned(projectile: Projectile) -> void:
@@ -14,3 +15,4 @@ func _on_spawner_scene_spawned(projectile: Projectile) -> void:
 	projectile.modulate = Color(0.8, 
 		0.8 - projectile.speed * COLOR_MODDLATE_FACTOR, 
 		0.8 - projectile.speed * COLOR_MODDLATE_FACTOR)
+	projectile.max_distance_from_axis = max_distance_from_axis
