b7925f89c3c69304abc10b4e25133503b88a0807
SytonK <122539345+SytonK@users.noreply.github.com>
2024-05-09 21:14:09 +0300

    PauseMenu/add pause button

--- src/UI/menus/pause menu/pause_menu.gd
+++ src/UI/menus/pause menu/pause_menu.gd
@@ -6,6 +6,12 @@ extends Control
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
-		print('lama')
+		_toggle_pause()
+
+func _toggle_pause() -> void:
 		visible = !visible
 		get_tree().paused = !get_tree().paused
+
+
+func _on_pause_button_pressed() -> void:
+	_toggle_pause()
