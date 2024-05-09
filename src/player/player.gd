12af2af6a5ae85ba80d6f851405a4ff097d57a57
SytonK <122539345+SytonK@users.noreply.github.com>
2024-05-09 08:23:41 +0300

    HealthUi/create

--- src/player/player.gd
+++ src/player/player.gd
@@ -4,8 +4,10 @@ extends CharacterBody2D
 
 @export var movement_speed: float = 500
 
-var max_health: int = 3
+var max_health: int = 4
 var health: int = max_health : set = _set_health
+@onready var health_ui: HealthUI = $CanvasLayer/HealthUI
+
 
 func _ready() -> void:
 	Globals.player_ref = self
@@ -26,6 +28,7 @@ func _on_hurtbox_hurt() -> void:
 
 func _set_health(new_health: int) -> void:
 	health = clamp(new_health, 0, max_health)
+	health_ui.new_health(health)
 	if health <= 0:
 		_die()
 
