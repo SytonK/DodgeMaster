16b930662e3dd0b115b39bc675ca347be93dcd43
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-05-11 12:00:38 +0300

    viewport

--- src/UI/menus/menu components/main menu button/main_menu_button.gd
+++ src/UI/menus/menu components/main menu button/main_menu_button.gd
@@ -2,5 +2,5 @@ extends Button
 
 
 func _on_pressed() -> void:
-	get_tree().paused = false
+	get_tree().paused = false                 
 	get_tree().change_scene_to_file("res://src/UI/menus/main menu/main_menu.tscn")
