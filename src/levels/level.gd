9e9351c1b18a130fc2a6ed7faf7d6ff5fa22a684
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-05-14 00:13:34 +0300

    Level1/init

--- src/levels/level.gd
+++ src/levels/level.gd
@@ -6,6 +6,13 @@ extends Node2D
 @onready var pause_menu: Control = $LevelUi/PauseMenu
 
 
+@onready var player: Player = $Player
+
+
+func _ready() -> void:
+	player.game_over.connect(_on_player_game_over)
+
+
 func _on_player_game_over() -> void:
 	game_over_menu.visible = true
 	get_tree().paused = true
