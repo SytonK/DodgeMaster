17998725513479de3199bf481d0b11786732cf00
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-05-14 03:13:57 +0300

    Level/add pleyer and level ui

--- src/levels/level.gd
+++ src/levels/level.gd
@@ -2,15 +2,28 @@ class_name Level
 extends Node2D
 
 
-@onready var game_over_menu: Control = $LevelUi/GameOverMenu
-@onready var pause_menu: Control = $LevelUi/PauseMenu
+const LEVEL_UI = preload("res://src/UI/level ui/level_ui.tscn")
+@onready var game_over_menu: Control
+@onready var pause_menu: Control
 
 
-@onready var player: Player = $Player
+const PLAYER = preload("res://src/player/player.tscn")
+@onready var player: Player
 
 
 func _ready() -> void:
+	_add_player()
+	_add_level_ui()
+
+func _add_player() -> void:
+	player = PLAYER.instantiate()
+	add_child(player)
 	player.game_over.connect(_on_player_game_over)
+
+func _add_level_ui() -> void:
+	add_child(LEVEL_UI.instantiate())
+	game_over_menu = $LevelUi/GameOverMenu
+	pause_menu = $LevelUi/PauseMenu
 
 
 func _on_player_game_over() -> void:
