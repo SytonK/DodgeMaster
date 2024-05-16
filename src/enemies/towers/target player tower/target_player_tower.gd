d8bff1d54438fd49fe9187a48905159311a8fe73
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-05-16 08:16:16 +0300

    Tower/add disable mode

--- src/enemies/towers/target player tower/target_player_tower.gd
+++ src/enemies/towers/target player tower/target_player_tower.gd
@@ -1,7 +1,13 @@
-extends Node2D
+extends StaticBody2D
 
 
-const COLOR_MODDLATE_FACTOR: float = 0.001
+const SHOOT_COLOR_MODDLATE_FACTOR: float = 0.001
+
+
+
+@export var disabled: bool = false : set = _on_set_disable
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 @onready var spawner: Spawner = $Spawner
 
@@ -11,6 +17,7 @@ const COLOR_MODDLATE_FACTOR: float = 0.001
 
 
 func _ready() -> void:
+	_on_set_disable(disabled)
 	spawner.frequency = frequency
 
 
@@ -19,6 +26,14 @@ func _on_spawner_scene_spawned(projectile: Projectile) -> void:
 	projectile.rotation = projectile.direction.angle()
 	projectile.speed = projectile_speed
 	projectile.modulate = Color(0.8, 
-		0.8 - projectile.speed * COLOR_MODDLATE_FACTOR, 
-		0.8 - projectile.speed * COLOR_MODDLATE_FACTOR)
+		0.8 - projectile.speed * SHOOT_COLOR_MODDLATE_FACTOR, 
+		0.8 - projectile.speed * SHOOT_COLOR_MODDLATE_FACTOR)
 	projectile.max_distance_from_axis = max_distance_from_axis
+
+
+func _on_set_disable(new_val: bool) -> void:
+	disabled = new_val
+	if spawner:
+		spawner.process_mode = Node.PROCESS_MODE_DISABLED if disabled else Node.PROCESS_MODE_INHERIT
+	if sprite_2d:
+		sprite_2d.self_modulate = Color(0.5, 0.5, 0.5) if disabled else Color(0.8, 0.5, 0.5)
