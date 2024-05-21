087355ed34ef311de09f10983c0f30b2fa6bf25f
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-05-22 00:58:34 +0300

    Level/time label colorrs

--- src/levels/level.gd
+++ src/levels/level.gd
@@ -72,10 +72,10 @@ func _on_level_next_index() -> void:
 			_on_hard_mode_enter()
 
 func _on_normal_mode_enter() -> void:
-	time_label.set("theme_override_colors/font_color", Color(0.8, 0, 0))
+	time_label.set("theme_override_colors/font_color", Color(0.8, 0.8, 0))
 
 func _on_hard_mode_enter() -> void:
-	time_label.set("theme_override_colors/font_color", Color(0.8, 0.8, 0))
+	time_label.set("theme_override_colors/font_color", Color(0.8, 0, 0))
 
 func _on_end_mode_enter() -> void:
 	#change this code for the level
