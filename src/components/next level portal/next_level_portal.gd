0e9b74d3d78ca5941a1e87c793aa05906e59a51d
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-05-26 23:00:21 +0300

    NextLevelPortal/functionality

--- src/components/next level portal/next_level_portal.gd
+++ src/components/next level portal/next_level_portal.gd
@@ -1,5 +1,9 @@
 extends Area2D
 
+@export var next_level_number: int
 
 func _on_body_entered(_player: Player) -> void:
-	print('next level')
+	call_deferred("change_scene")
+
+func change_scene() -> void:
+	get_tree().change_scene_to_file("res://src/levels/level " + str(next_level_number) + "/level_" + str(next_level_number) + ".tscn")
